6ce342277caafb595c41ea1bd46da98890c165f3
Rafee <rafeehasan88@gmail.com>
2026-01-09 23:03:16 +0600

    basic set up and lightweight animation for register

---
+++ src/router/router.jsx
@@ -0,0 +1,25 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout> ,
+    errorElement: <h2 className="text-4xl text-center">Route not found</h2>,
+    children:[
+        {
+            path:"/",
+            element:<Home></Home>
+        },
+        {
+            path:"register",
+            element: <Register></Register>
+        }
+    ]
+  },
+]);
+export default router;
