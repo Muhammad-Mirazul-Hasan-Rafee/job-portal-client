e2835985253947c00a484c22ca9e0732a0ca5754
Rafee <rafeehasan88@gmail.com>
2026-01-12 19:23:32 +0600

    added animation of imags with motion

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,19 +1,29 @@
 import { easeOut, motion } from "motion/react";
 
+import happyTeam from '../../assets/Team/happy-team.jpg';
+import workingTeam from '../../assets/Team/working-team.jpg';
+
+
 const Banner = () => {
   return (
     <div className="hero bg-base-200 min-h-96">
       <div className="hero-content flex-col lg:flex-row-reverse">
        <div className="flex-1">
-         <img
-          src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-          className="max-w-sm rounded-lg shadow-2xl"
+         <motion.img
+          src={happyTeam}
+          animate={{y: [50 , 120 , 50]}} transition={{duration:10, repeat:Infinity}}
+          className="w-60 h-48 rounded-t-[38px] rounded-br-[38px] border-l-3 border-b-3 border-blue-300 shadow-2xl"
+        />
+         <motion.img
+          src={workingTeam}
+          animate={{x: [100 , 160 , 100]}} transition={{duration:10, repeat:Infinity}}
+          className="w-60 h-48 rounded-t-[38px] rounded-bl-[38px] border-r-3 border-b-3 border-blue-300 shadow-2xl"
         />
        </div>
         <div className="flex-1">
-          <motion.h1 animate={{x:150, color: ['white', 'green']}}
-          transition={{duration:2.5 , delay:1 , ease: easeOut, repeat:Infinity}}
-           className="text-5xl font-bold">Latest jobs!</motion.h1>
+          <motion.h1 animate={{x:150}}
+          transition={{duration:1.7 , delay:1 , ease: easeOut, repeat:Infinity}}
+           className="text-5xl font-bold"> Your latest <motion.span  animate={{color:['#D9994C' , '#47C94D' , '06083B' , '#5D66F0']}} transition={{duration:1.6, repeat:Infinity}}>jobs!</motion.span></motion.h1>
           <p className="py-6">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
