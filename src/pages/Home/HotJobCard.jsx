c82dedc21238e0b6cda95dd5aeb780870853c506
Rafee <rafeehasan88@gmail.com>
2026-01-14 00:43:46 +0600

    fetched all data in the home component and distructured the points and also set

---
+++ src/pages/Home/HotJobCard.jsx
@@ -0,0 +1,44 @@
+import { FaMapMarkerAlt } from "react-icons/fa";
+
+const HotJobCard = ({ job }) => {
+  const {
+    title,
+    company,
+    company_logo,
+    requirements,
+    description,
+    location,
+    salaryRange,
+  } = job;
+  return (
+    <div className="card bg-base-100  shadow-sm">
+<div className="flex gap-2 m-2">
+          <figure>
+        <img className="w-16"
+          src={company_logo}
+          alt={title}
+        />
+      </figure>
+      <aside>
+        <h4 className="text-2xl">{company}</h4>
+        <p className="flex gap-1  items-center"><FaMapMarkerAlt />{location}</p>
+      </aside>
+</div>
+      <div className="card-body">
+        <h2 className="card-title">{title}
+            <div className="badge badge-secondary">New</div>
+        </h2>
+        <p>
+         {description}
+        </p>
+        <div className="flex gap-2 flex-wrap">{requirements.map((requirement)=> <p className="border rounded-md text-center px-2 hover:text-yellow-500 hover:bg-slate-900">{requirement}</p>)}</div>
+        <div className="card-actions justify-end items-center mt-4">
+            <p>Salary: {salaryRange.min} - {salaryRange.max} {salaryRange.currency}</p>
+          <button className="btn btn-primary">Apply</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HotJobCard;
